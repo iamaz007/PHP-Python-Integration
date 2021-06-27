1598f9b6f40a2b79ccc04151918c386d36b06b72
Abdullah Zahid <alexmercer028@gmail.com>
2021-06-27 19:06:16 +0500

    minor changes

--- php/functions.php
+++ php/functions.php
@@ -5,9 +5,8 @@
         $url = "http://127.0.0.1:5000/".$_POST['dataType'];
         // $url = "http://127.0.0.1:5000/women-cloths";
         $data = file_get_contents($url);
-
+        echo $data;
         $res = json_decode(mb_convert_encoding($data,'UTF-8','UTF-8'), true);
-
 
         // inserting in  women_cloths
         $temp = $res['list'];
@@ -21,15 +20,16 @@
                 VALUES ('".$title."', '".$price."', '".$image."')";
 
                 if ($con->query($sql) === TRUE) {
-                    echo "New record created successfully";
+                    // echo "New record created successfully";
                 } else {
-                    echo "Error: " . $sql . "<br>" . $con->error;
+                    // echo "Error: " . $sql . "<br>" . $con->error;
                 }
 
             }
         }
 
         $con->close();
+        
     }
     
 ?>
